c81a4d7ba7e4639eb28a70a0cae2f1ad3df4c863
jooho172200 <jooho172200@gmail.com>
2025-05-23 00:40:33 +0900

    feat: 지도 및 낙석 카드 기능 구현 및 예외 코드 추가

--- src/main/java/com/goldstone/saboteur_backend/domain/card/ActionCard.java
+++ src/main/java/com/goldstone/saboteur_backend/domain/card/ActionCard.java
@@ -1,7 +1,11 @@
 package com.goldstone.saboteur_backend.domain.card;
 
+import com.goldstone.saboteur_backend.domain.board.Cell;
 import com.goldstone.saboteur_backend.domain.enums.ActionCardType;
+import com.goldstone.saboteur_backend.domain.enums.GoalCardType;
 import com.goldstone.saboteur_backend.domain.enums.TargetToolType;
+import com.goldstone.saboteur_backend.exception.BusinessException;
+import com.goldstone.saboteur_backend.exception.code.error.CardErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,7 +27,29 @@ public class ActionCard extends Card {
 
     // repairTool
     // destroyTool
+
     // fallingRock
+    public void fallingRock(Cell targetCell){
+        if(this.type != ActionCardType.FALLING_ROCK){
+            throw new BusinessException(CardErrorCode.INVALID_ACTION_CARD);
+        }
+
+        targetCell.removeCard();
+    }
+
+
     // peekDestinationCard
+    public GoalCardType peekDestinationCard(Cell targetCell){
+        if(this.type != ActionCardType.MAP){
+            throw new BusinessException(CardErrorCode.INVALID_ACTION_CARD);
+        }
+
+        if(!(targetCell.getCard() instanceof GoalCard) || targetCell == null || targetCell.isEmptyCard()){
+            throw new BusinessException(CardErrorCode.INVALID_GOAL_CARD);
+        }
+
+        GoalCard goalCard = (GoalCard) targetCell.getCard();
+        return goalCard.getType();
+    }
 
 }
