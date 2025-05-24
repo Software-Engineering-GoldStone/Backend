9a746b4a70a67343b11d91444fa1730b8d9955a9
jooho172200 <jooho172200@gmail.com>
2025-05-24 20:12:13 +0900

    ActionCard.java 수정

--- src/main/java/com/goldstone/saboteur_backend/domain/card/ActionCard.java
+++ src/main/java/com/goldstone/saboteur_backend/domain/card/ActionCard.java
@@ -6,19 +6,31 @@ import com.goldstone.saboteur_backend.domain.enums.GoalCardType;
 import com.goldstone.saboteur_backend.domain.enums.TargetToolType;
 import com.goldstone.saboteur_backend.exception.BusinessException;
 import com.goldstone.saboteur_backend.exception.code.error.CardErrorCode;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ActionCard extends Card {
     private ActionCardType type;
     private TargetToolType tool;
+    private Cell targetCell;
+    private Set<TargetToolType> tools = new HashSet<>();
 
     @Override
-    public void use() {}
+    public void use() {
+        switch (type) {
+            case FALLING_ROCK -> targetCell.removeCard();
+            case MAP -> peekDestinationCard(targetCell);
+            default -> throw new BusinessException(CardErrorCode.INVALID_ACTION_CARD);
+        }
+    }
 
     @Override
     public boolean availableUse() {
@@ -26,22 +38,23 @@ public class ActionCard extends Card {
     }
 
     // repairTool
-    // destroyTool
-
-    // fallingRock
-    public void fallingRock(Cell targetCell) {
-        if (this.type != ActionCardType.FALLING_ROCK) {
-            throw new BusinessException(CardErrorCode.INVALID_ACTION_CARD);
+    public void repairTool(Set<TargetToolType> tools) {
+        for (TargetToolType tool : tools) {
+            if (this.tools.contains(tool)) {
+                this.tools.remove(tool);
+            } else {
+                this.tools.add(tool);
+            }
         }
+    }
 
-        targetCell.removeCard();
+    // destroyTool
+    public void destoryTool(TargetToolType tool) {
+        // 유저 길놓기 비활성화?
     }
 
     // peekDestinationCard
     public GoalCardType peekDestinationCard(Cell targetCell) {
-        if (this.type != ActionCardType.MAP) {
-            throw new BusinessException(CardErrorCode.INVALID_ACTION_CARD);
-        }
 
         if (!(targetCell.getCard() instanceof GoalCard)
                 || targetCell == null
