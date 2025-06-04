651a107304f7c2512490146115c705690c3b05af
박지호 <jiho5993@gmail.com>
2025-06-04 12:57:18 +0000

    fix: PathValidator isConnected 버그 수정

--- src/main/java/com/goldstone/saboteur_backend/domain/board/Board.java
+++ src/main/java/com/goldstone/saboteur_backend/domain/board/Board.java
@@ -91,9 +91,9 @@ public class Board {
         int dy = toCell.getY() - fromCell.getY();
 
         int direction = -1;
-        if (dx == 0 && dy == -1) direction = 0; // 위
+        if (dx == 0 && dy == 1) direction = 0; // 위
         else if (dx == 1 && dy == 0) direction = 1; // 오른쪽
-        else if (dx == 0 && dy == 1) direction = 2; // 아래
+        else if (dx == 0 && dy == -1) direction = 2; // 아래
         else if (dx == -1 && dy == 0) direction = 3; // 왼쪽
 
         if (direction == -1) return false; // 인접하지 않은 셀
