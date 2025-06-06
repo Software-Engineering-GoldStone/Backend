5f0527786e71d61283f0cb17f5c6c602c669b41d
박지호 <jiho5993@gmail.com>
2025-06-06 17:52:27 +0000

    feat: getBoardInfo 이벤트 구현

--- src/main/java/com/goldstone/saboteur_backend/dtos/cell/response/CellInfoResponseDto.java
+++ src/main/java/com/goldstone/saboteur_backend/dtos/cell/response/CellInfoResponseDto.java
@@ -1,7 +1,9 @@
 package com.goldstone.saboteur_backend.dtos.cell.response;
 
 import com.goldstone.saboteur_backend.domain.board.Cell;
+import com.goldstone.saboteur_backend.domain.card.PathCard;
 import com.goldstone.saboteur_backend.domain.enums.PathType;
+import com.goldstone.saboteur_backend.dtos.card.response.PathCardInfoResponseDto;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,12 +12,17 @@ import lombok.Getter;
 public class CellInfoResponseDto {
     private final int x;
     private final int y;
+    private final PathCardInfoResponseDto card;
     private final PathType[] sides;
 
     public static CellInfoResponseDto from(Cell cell) {
         return CellInfoResponseDto.builder()
                 .x(cell.getX())
                 .y(cell.getY())
+                .card(
+                        cell.getCard() == null
+                                ? null
+                                : PathCardInfoResponseDto.from((PathCard) cell.getCard()))
                 .sides(cell.getSides())
                 .build();
     }
--- src/main/java/com/goldstone/saboteur_backend/socketIo/eventRegister/BoardEventRegister.java
+++ src/main/java/com/goldstone/saboteur_backend/socketIo/eventRegister/BoardEventRegister.java
@@ -4,7 +4,9 @@ import com.corundumstudio.socketio.SocketIOServer;
 import com.goldstone.saboteur_backend.domain.board.Board;
 import com.goldstone.saboteur_backend.domain.board.Cell;
 import com.goldstone.saboteur_backend.domain.game.GameRoom;
+import com.goldstone.saboteur_backend.dtos.board.request.GetBoardInfoRequestDto;
 import com.goldstone.saboteur_backend.dtos.board.request.GetReachableGoalsRequestDto;
+import com.goldstone.saboteur_backend.dtos.board.response.BoardInfoResponseDto;
 import com.goldstone.saboteur_backend.dtos.board.response.GetReachableGoalsResponseDto;
 import com.goldstone.saboteur_backend.exception.BusinessException;
 import com.goldstone.saboteur_backend.exception.code.error.ErrorCode;
@@ -25,6 +27,7 @@ public class BoardEventRegister implements SocketEventRegister {
 
     @Override
     public void registerEvents(SocketIOServer server) {
+        // 도착지 판별
         server.addEventListener(
                 "getReachableGoals",
                 GetReachableGoalsRequestDto.class,
@@ -45,6 +48,33 @@ public class BoardEventRegister implements SocketEventRegister {
                         client.sendEvent(
                                 "reachableGoals",
                                 GetReachableGoalsResponseDto.of(gameRoom, result));
+                    } catch (Exception e) {
+                        if (e instanceof BusinessException) {
+                            ErrorCode errorCode = ((BusinessException) e).getErrorCode();
+                            client.sendEvent("errorEvent", new ErrorResponse(errorCode));
+                        } else {
+                            client.sendEvent("errorEvent", ErrorResponse.internalServerError());
+                        }
+                    }
+                });
+
+        // 보드 정보 반환
+        server.addEventListener(
+                "getBoardInfo",
+                GetBoardInfoRequestDto.class,
+                (client, request, ackSender) -> {
+                    try {
+                        GameRoom gameRoom =
+                                this.globalSession.getGameRoomSession(request.getGameRoomId());
+                        if (gameRoom == null) {
+                            throw new BusinessException(GameRoomErrorCode.GAME_ROOM_NOT_FOUND);
+                        }
+                        Board board = this.globalSession.getGameBoardSession(gameRoom.getId());
+                        if (board == null) {
+                            throw new BusinessException(GameRoomErrorCode.GAME_BOARD_NOT_FOUND);
+                        }
+
+                        client.sendEvent("boardInfo", BoardInfoResponseDto.from(board));
                     } catch (Exception e) {
                         if (e instanceof BusinessException) {
                             ErrorCode errorCode = ((BusinessException) e).getErrorCode();
